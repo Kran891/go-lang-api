6e939bde321b8e82502c4a56e2ef7c622493fce9
kranthi-gavireddy <kranthi.gavireddy@wbdcontractor.com>
2024-12-25 18:25:19 +0530

    commit

--- main/main.go
+++ main/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/kran891/go-lang-api/common/errors"
+)
+
+func main() {
+	k := errors.New("Unknown Error")
+	z := errors.Wrap(k, "Hello")
+	m := errors.UnWrap(z)
+	fmt.Print(m)
 }
